b4317880e93e04afbe519dd7edcfa93ed0fb8bcd
jayachithra <s.k.jayachithra@gmail.com>
2018-04-08 16:21:23 +0200

    Update for Python 3
    The source code is now compatible for python version 3 in addition to 2. Files updated -api/examples/albums.py -api/examples/artists.py -api/examples/tracks.py -api/spotify.py -spotify_requests/spotify.py

--- api/examples/albums.py
+++ api/examples/albums.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 
 sys.path.append('../')
@@ -10,34 +11,34 @@ results = spotify.search(['album'], 'Currents')
 
 # results is a nest dictionary
 # if you want to see with your eyes...
-print results
+print(results)
 
 # lets see the results and choose the first result as our correct album
 # with enough info probably spotify api will rank what you're looking for
 # correcty
-print 'Search results:'
-print '-------------------------------------------------------'
+print('Search results:')
+print('-------------------------------------------------------')
 albums = results['albums']['items']
 for a in albums:
-    print '%s by %s' % (a['name'], a['artists'][0]['name'])
-print
-print
+    print('%s by %s' % (a['name'], a['artists'][0]['name']))
+print()
+print()
 
 # WE HAVE THE ALBUM :D! And now we can make more awesome things with it
 album_id = albums[0]['id']
 
 # I want to know more about this album... let me ask spotify
 album_info = spotify.get_album(album_id)
-print 'Info about Currents by Tame Impala'
-print '--------------------------------------------------------'
-print 'name:', album_info['name']
-print 'release date:', album_info['release_date']
-print 'populariy:', album_info['popularity']
-print
-print
+print('Info about Currents by Tame Impala')
+print('--------------------------------------------------------')
+print('name:', album_info['name'])
+print('release date:', album_info['release_date'])
+print('populariy:', album_info['popularity'])
+print()
+print()
 
-print 'Tracks'
-print '--------------------------------------------------------'
+print('Tracks')
+print('--------------------------------------------------------')
 for track in spotify.get_albums_tracks(album_id)['items']:
-    print track['name']
+    print(track['name'])
 
--- api/examples/artists.py
+++ api/examples/artists.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 
 sys.path.append('../')
@@ -10,49 +11,49 @@ results = spotify.search(['artist'], 'Michael Jackson')
 
 # results is a nest dictionary
 # if you want to see with your eyes...
-print results
+print(results)
 
 # lets see the results and choose the first result as our correct artist
 # with enough info probably spotify api will rank what you're looking for
 # correcty
-print 'Search results:'
-print '-------------------------------------------------------'
+print('Search results:')
+print('-------------------------------------------------------')
 artists = results['artists']['items']
-for a in artists: print '%s, popularity: %s' % (a['name'], a['popularity'])
-print
-print
+for a in artists: print('%s, popularity: %s' % (a['name'], a['popularity']))
+print()
+print()
 
 # WE HAVE THE ARTIST :D! And now we can make more awesome things with it
 artist_id = artists[0]['id']
 
 # I want to know more about this artist... let me ask spotify
 artist_info = spotify.get_artist(artist_id)
-print 'Info about Michael Jackson:'
-print '--------------------------------------------------------'
-print 'name:', artist_info['name']
-print 'populariy:', artist_info['popularity']
-print 'num. followers:', artist_info['followers']['total']
-print
-print
+print('Info about Michael Jackson:')
+print('--------------------------------------------------------')
+print('name:', artist_info['name'])
+print('populariy:', artist_info['popularity'])
+print('num. followers:', artist_info['followers']['total'])
+print()
+print()
 
-print 'Albums'
-print '--------------------------------------------------------'
+print('Albums')
+print('--------------------------------------------------------')
 for album in spotify.get_artists_albums(artist_id)['items']:
-    print album['name']
-print
-print
+    print(album['name'])
+print()
+print()
 
-print 'Top Tracks'
-print '--------------------------------------------------------'
+print('Top Tracks')
+print('--------------------------------------------------------')
 for track in spotify.get_artists_top_tracks(artist_id)['tracks']:
-    print track['name']
-print
-print
+    print(track['name'])
+print()
+print()
 
-print 'Related Artists'
-print '--------------------------------------------------------'
+print('Related Artists')
+print('--------------------------------------------------------')
 for ra in spotify.get_related_artists(artist_id)['artists']:
-    print ra['name']
+    print(ra['name'])
 
 
 
